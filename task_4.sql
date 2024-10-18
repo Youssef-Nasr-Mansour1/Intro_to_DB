68c61b4c26991eb21174a3af4c1954035ad5c1fe
Youssef-Nasr-Mansour <youssefnasr852@gmail.com>
2024-10-18 18:29:44 +0300

    SELECT

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,17 @@
 SELECT 
-    CONCAT('SHOW CREATE TABLE ', 'books') AS 'Query'
-INTO @sql_query;
-
-PREPARE stmt FROM @sql_query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+    T.TABLE_NAME AS 'Table',
+    C.COLUMN_NAME AS 'Field',
+    C.COLUMN_TYPE AS 'Type',
+    C.IS_NULLABLE AS 'Null',
+    C.COLUMN_KEY AS 'Key',
+    C.COLUMN_DEFAULT AS 'Default',
+    C.EXTRA AS 'Extra'
+FROM 
+    information_schema.tables T
+JOIN 
+    information_schema.columns C 
+ON 
+    T.TABLE_NAME = C.TABLE_NAME
+WHERE 
+    T.TABLE_SCHEMA = 'alx_book_store'
+    AND T.TABLE_NAME = 'books';
