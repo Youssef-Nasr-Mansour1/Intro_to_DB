e3ee9c4d9883a0a8f228a4a1767425606d6114d5
Youssef-Nasr-Mansour <youssefnasr852@gmail.com>
2024-10-18 18:34:21 +0300

    USE alx_book_store;

--- task_4.sql
+++ task_4.sql
@@ -1,23 +1,3 @@
-SELECT 
-    T.TABLE_NAME AS 'Table',
-    C.COLUMN_NAME AS 'Field',
-    C.COLUMN_TYPE AS 'Type',
-    C.IS_NULLABLE AS 'Null',
-    C.COLUMN_KEY AS 'Key',
-    C.COLUMN_DEFAULT AS 'Default',
-    C.EXTRA AS 'Extra'
-FROM 
-    information_schema.tables T
-JOIN 
-    information_schema.columns C 
-ON 
-    T.TABLE_NAME = C.TABLE_NAME
-WHERE 
-    T.TABLE_SCHEMA = 'alx_book_store'
-    AND T.TABLE_NAME = 'books';
-SET @table_name = 'books';
+USE alx_book_store;
 
-SET @sql = CONCAT('SHOW CREATE TABLE ', @table_name);
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+SHOW COLUMNS FROM books;
